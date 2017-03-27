5bd4c09bed461d799cfb61cb7e7144b91193b842
Dirk Peters <dpeters@3fish.org>
2017-03-27 19:53:57 +0200

    Initial Commit

---
+++ conf/default.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Default settings for the ldapdisplay plugin
+ *
+ * @author Dirk Peters <peters@code-maze.de>
+ */
+
+$conf['serviceUserName']    = '';
+$conf['serviceUserPass']    = '';
+$conf['rootDir']            = '';
+$conf['limitFieldsTo']      = 'cn,sn,mail';
---
+++ conf/metadata.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Options for the ldapdisplay plugin
+ *
+ * @author Dirk Peters <peters@code-maze.de>
+ */
+
+
+$meta['serviceUserName'] = array('string');
+$meta['serviceUserPass'] = array('string');
+$meta['rootDir'] = array('string');
+$meta['limitFieldsTo'] = array('multichoice','_choices');
+
